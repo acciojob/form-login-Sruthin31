673a05c8af148241ef604331da3a89e04669f695
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-01-12 08:53:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	
+	const firstName = document.querySelector('input[name="fname"]').value;
+	const lastName = document.querySelector('input[name="lname"]').value;
 
+	
+
+	alert(`${firstName} ${lastName}`);
+	
 }
